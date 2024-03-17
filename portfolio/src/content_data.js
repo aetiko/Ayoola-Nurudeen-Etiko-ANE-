0d73acc4af0beb615b1565dda919b863738f0fac
ayo <nurudeenetiko12@gmail.com>
2024-03-17 03:08:02 -0600

    here is my data

--- portfolio/src/content_data.js
+++ portfolio/src/content_data.js
@@ -1,7 +1,7 @@
-const logotext = "JOHN";
+const logotext = "NURUDEEN";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+    title: "Nurudeen Etiko",
+    description: "I’m Nurudeen Etiko data scientist _ Full stack devloper,currently working in Berlin",
 };
 
 const introdata = {
